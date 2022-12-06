7f51e5e9b77432bf23e1b9202ad1e1818c831300
chase-manning <chase@manning.dev>
2022-12-06 12:06:32 +0000

    :wrench: add example secrets file

---
+++ example.secrets.js
@@ -0,0 +1,9 @@
+// Rename this to `secrets.js` and fill in the values
+
+const ALCHEMY_KEY = ""; // Add your Alchemy API key here
+const INFURA_KEY = ""; // Add your Infura API key here
+const LLAMA_NODES_KEY = ""; // Add your Llama Nodes API key here
+
+exports.ALCHEMY_KEY = ALCHEMY_KEY;
+exports.INFURA_KEY = INFURA_KEY;
+exports.LLAMA_NODES_KEY = LLAMA_NODES_KEY;
